537f060584f0da45f26856d6f032517569a45257
dwepCC <angelinos257@gmail.com>
2021-07-16 10:39:53 -0500

    arcgivos subidos

---
+++ Config/config.php
@@ -0,0 +1,13 @@
+<?php
+//varibles de coneccion
+    define("HOST",'locaLhost');
+    define("DB_USER",'root');
+    define("DB_PASS",'');
+    define("DB_NAME",'prestacenter');
+
+    define("PORT",3306);
+    define("CHARSET",'utf8');
+    //define("UNIX_SOCKET",'/var/run/mysql/mysql.sock');
+    define("SYSTEMNAME","PrestaYa");
+
+
